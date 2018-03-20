0277ff4863fd7ac45493ffc657bdf1f5c2c599c3
zhonggan <644635057@qq.com>
2018-03-20 13:21:15 +0800

    s
    s

--- Go_grountime/src/go_main.go
+++ Go_grountime/src/go_main.go
@@ -16,3 +16,16 @@ hello Go
 func helloWord() {
 	fmt.Print("hellow Go\n")
 }
+
+type ClassTest struct {
+	isParivate int
+	Ispublic   int
+	d          int
+}
+
+/**
+测试2
+**/
+func ClassTestPublicMeth(b *ClassTest) {
+
+}
